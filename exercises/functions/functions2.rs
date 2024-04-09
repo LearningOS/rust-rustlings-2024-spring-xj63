71e2e49150876fccf2689f112c07079269be011c
xj63 <xj63work@163.com>
2024-04-09 11:28:25 +0800

    partial: functions/functions2

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -3,14 +3,12 @@
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
+fn call_me(num: usize) {
+    for i in 1..=num {
+        println!("Ring! Call number {}", i);
     }
 }
