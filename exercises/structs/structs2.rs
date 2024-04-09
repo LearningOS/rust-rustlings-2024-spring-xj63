27e91f9a12762b3327985030831e2f379c4a492c
xj63 <xj63work@163.com>
2024-04-09 16:07:35 +0800

    partial: structs/structs2

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -16,6 +14,28 @@ struct Order {
     made_by_email: bool,
     item_number: u32,
     count: u32,
+}
+
+impl Order {
+    fn new(
+        name: String,
+        year: u32,
+        made_by_phone: bool,
+        made_by_mobile: bool,
+        made_by_email: bool,
+        item_number: u32,
+        count: u32,
+    ) -> Self {
+        Order {
+            name,
+            year,
+            made_by_phone,
+            made_by_mobile,
+            made_by_email,
+            item_number,
+            count,
+        }
+    }
 }
 
 fn create_order_template() -> Order {
@@ -37,8 +57,12 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
-        // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        // Create your own order using the update syntax and template above!
+        let your_order = Order {
+            name: "Hacker in Rust".to_string(),
+            count: 1,
+            ..order_template
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
