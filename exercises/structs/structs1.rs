fc94ba720ea5833b3821826c5fbe2feab0834ec4
xj63 <xj63work@163.com>
2024-04-09 16:02:30 +0800

    partial: structs/structs1

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,13 +5,60 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-struct ColorClassicStruct {
-    // TODO: Something goes here
+trait NewColor
+where
+    Self: Sized,
+{
+    fn new(red: u8, green: u8, blue: u8) -> Self;
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+trait DefaultColor: NewColor {
+    fn default_red() -> Self {
+        Self::new(255, 0, 0)
+    }
+
+    fn default_green() -> Self {
+        Self::new(0, 255, 0)
+    }
+
+    fn default_blue() -> Self {
+        Self::new(0, 0, 255)
+    }
+
+    fn default_black() -> Self {
+        Self::new(0, 0, 0)
+    }
+
+    fn default_white() -> Self {
+        Self::new(255, 255, 255)
+    }
+}
+
+struct ColorClassicStruct {
+    // Something goes here
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl NewColor for ColorClassicStruct {
+    fn new(red: u8, green: u8, blue: u8) -> Self {
+        ColorClassicStruct { red, green, blue }
+    }
+}
+
+impl DefaultColor for ColorClassicStruct {}
+
+/*  Something goes here */
+struct ColorTupleStruct(u8, u8, u8);
+
+impl NewColor for ColorTupleStruct {
+    fn new(red: u8, green: u8, blue: u8) -> Self {
+        ColorTupleStruct(red, green, blue)
+    }
+}
+
+impl DefaultColor for ColorTupleStruct {}
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -22,8 +69,8 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        // Instantiate a classic c struct!
+        let green = ColorClassicStruct::default_green();
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -32,8 +79,8 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
+        // Instantiate a tuple struct!
+        let green = ColorTupleStruct::default_green();
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -42,8 +89,8 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        // Instantiate a unit-like struct!
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
