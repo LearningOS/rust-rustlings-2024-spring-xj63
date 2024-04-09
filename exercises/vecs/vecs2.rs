3949e1a05b5e148eb9e192287ab58efbee76d772
xj63 <xj63work@163.com>
2024-04-09 12:11:23 +0800

    completed: vecs

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -7,25 +7,19 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for element in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
-        ???
-    }
+    // Fill this up so that each element in the Vec `v` is
+    // multiplied by 2.
+    v.iter_mut().for_each(|n| *n *= 2);
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
     v
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        ???
-    }).collect()
+    // Do the same thing as above - but instead of mutating the
+    // Vec, you can just return the new number!
+    v.iter().map(|x| x * 2).collect()
 }
 
 #[cfg(test)]
