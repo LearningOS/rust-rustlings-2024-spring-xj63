cf81521c4a4bec8fdbde7d46cdf3dc81e073e4ab
xj63 <xj63work@163.com>
2024-04-09 19:57:06 +0800

    completed: quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,9 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+trait Report {
+    fn print(&self) -> String;
+}
 
 pub struct ReportCard {
     pub grade: f32,
@@ -24,10 +26,38 @@ pub struct ReportCard {
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+impl Report for ReportCard {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+}
+
+struct ReportCard2 {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+fn grade2char(f: f32) -> &'static str {
+    match f {
+        v if v < 1.0 => panic!("grade less than 1.0"),
+        v if v <= 5.5 => "A+",
+        v if v <= 10.0  => "A",
+        _ => unimplemented!("and so on."),
+    }
+}
+
+impl Report for ReportCard2 {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            grade2char(self.grade)
+        )
     }
 }
 
@@ -50,8 +80,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
+        // Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard2 {
             grade: 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
