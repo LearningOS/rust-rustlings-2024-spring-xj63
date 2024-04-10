f5bece1eb795ce778cfb4f035639a21513f7dc9f
xj63 <xj63work@163.com>
2024-04-10 17:03:59 +0800

    partial: tests/tests7

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -14,11 +14,12 @@ fn main() {
         "Your command here with {}, please checkout exercises/tests/build.rs",
         timestamp
     );
-    println!("cargo:{}", your_command);
+    // println!("cargo:{}", your_command);
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
     let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    // println!("cargo:{}", your_command);
 }
