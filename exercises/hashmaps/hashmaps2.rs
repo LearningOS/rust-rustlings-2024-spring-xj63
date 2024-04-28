9a844c4d7cc9b66778c59d9e610c785e112116c2
xj63 <xj63work@163.com>
2024-04-29 00:09:33 +0800

    fix: hashmaps2 unuse feature.

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-#![feature(map_try_insert)]
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,10 +35,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
+        // Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
-        basket.try_insert(fruit, 1);
+        basket.entry(fruit).or_insert(1);
     }
 }
 
